d2411f9db61df595c62b98d145d364447422a43e
EC2 Default User <ec2-user@ip-172-31-42-27.ap-south-1.compute.internal>
2024-01-30 06:31:13 +0000

    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,14 +4,7 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
+    System.out.println("devops");
+    System.out.println("linux");
   }
 }
