b9320854b7ca980d59d64c2542d868d28e4a9b7f
EC2 Default User <ec2-user@ip-172-31-42-27.ap-south-1.compute.internal>
2024-01-30 11:48:48 +0000

    modified

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("nexus");
-    System.out.println("sonarqube");
+    System.out.println("devops");
   }
 }
