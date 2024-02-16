4c82fd04eded435e427c11d079e4ef035574be26
mashood6106 <157138690+mashood6106@users.noreply.github.com>
2024-02-16 14:03:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,9 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("Hello, World");
-    System.out.println("nexus");
-    System.out.println("jenkins job");
-    System.out.println("jenkins job");
-    System.out.println("jenkins job");
-    System.out.println("jenkins job");
-    System.out.println("jenkins job");
-    System.out.println("jenkins job");
-    System.out.println("jenkins job");
+     System.out.println("Hello, World");
+     System.out.println("Hello, World");
+     System.out.println("Hello, World");
+     System.out.println("Hello, World");
+     System.out.println("Hello, World");
   }
 }
